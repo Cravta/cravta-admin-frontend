ec7de32947a2521acec4763f6b8c700b99423921
matrix237 <57328672+Iam-mustafa@users.noreply.github.com>
2025-05-27 21:53:04 +0500

    tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react()],
-  server: { host: true, port: 3000 },
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    host: true,
+    port: 3000,
+  },
+});
